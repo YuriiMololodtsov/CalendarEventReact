96ce32c44c48e0b73189582e4b2f4a4b8d727e3e
YuriiMol <yuriimolodtsov@gmail.com>
2022-04-11 18:04:58 +0300

    addEvents

--- src/Note.js
+++ src/Note.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
-function Note() {
-  function setNote() {
-    console.log('qqqqq');
+function Note(props) {
+  const name = `${props.day} ${props.month} ${props.year}`;
+
+  const [value, setValue] = useState('');
+  const [text, setText] = useState('');
+
+  const [storage, setStorage] = useState({
+    name: [],
+  });
+  let res;
+  function handleClick() {
+    setText(value);
+
+    setStorage((storage) => ({
+      ...storage,
+      name: [...storage.name, text],
+    }));
+
+    localStorage.setItem(name, text);
   }
+  console.log(storage);
+
   return (
-    <div id="note">
+    <div className="note">
+      <h3 className="note__title">
+        {props.day} {props.month} {props.year}
+      </h3>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={handleClick}>Записать</button>
       <ol>
-        <ul>1</ul>
+        <li>{res}</li>
       </ol>
-      {setNote()}
     </div>
   );
 }
