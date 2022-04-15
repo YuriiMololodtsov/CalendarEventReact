967149fced2108ed13cd2a6bf72e3fa7efc167d8
YuriiMol <yuriimolodtsov@gmail.com>
2022-04-15 20:17:13 +0300

    update handle click, add events list

--- src/Note.js
+++ src/Note.js
@@ -6,23 +6,42 @@ function Note(props) {
 
   const [value, setValue] = useState();
   const [text, setText] = useState('');
-  const [test, setTest] = useState({});
-
   const [storage, setStorage] = useState({});
   let copy;
+
   console.log(text);
 
   function handleClick() {
     setText(value);
-
-    copy = Object.assign({}, storage);
-    copy[name] = [value];
-    setStorage(copy);
-
-    let json = JSON.stringify(copy[name]);
-
-    localStorage.setItem(name, json);
+    console.log(name);
+    if (name in storage) {
+      copy = Object.assign({}, storage);
+      copy[name] = [...copy[name], value];
+      setStorage(copy);
+      let json = JSON.stringify(copy[name]);
+      localStorage.setItem(name, json);
+      //добавить в массив storage[name], элемент
+      //сразу парсим сюда
+    } else {
+      copy = Object.assign({}, storage);
+      copy[name] = [value];
+      setStorage(copy);
+      let json = JSON.stringify(copy[name]);
+      localStorage.setItem(name, json);
+    }
   }
+  //создаем список событий из массива, крепим кнопку удаления
+  let res = JSON.parse(localStorage.getItem(name));
+  let viwe = res.map((item, index) => {
+    return (
+      <li key={index}>
+        {item}
+        <span>
+          <button>del</button>
+        </span>
+      </li>
+    );
+  });
   console.log(storage);
   return (
     <div className="note">
@@ -32,6 +51,7 @@ function Note(props) {
       <input value={value} onChange={(e) => setValue(e.target.value)} />
       <button onClick={handleClick}>Записать</button>
       <p>{JSON.parse(localStorage.getItem(name))}</p>
+      <ol>{viwe}</ol>
     </div>
   );
 }
